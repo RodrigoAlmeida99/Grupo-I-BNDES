3024699bfc33fd0ec1a7afeba5ff544de4354e78
Rodrigo Almeida <rsa.0408@hotmail.com>
2020-06-13 13:50:05 -0300

    alterando os alerts

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
 
-          
+       var cpf = document.getElementById("cpf"); 
        var nome = document.getElementById("nome");
 
-       var hospitalA;
-       var hospitalB;
-
-       hospitalA = 0;
-       hospitalB = 0;
-
-       if(hospitalA == 0){
-           console.log("Ä")
-
-       }else{
-
-            console.log("B")
-       }
+       var hospitalA = 0;
+       var hospitalB = 0;
+       var distanciaA = 5;
+       var distanciaB = 8;
 
 
-
-
-
-
-
+        
                var botao = document.getElementById("btn");
 
-            botao.addEventListener('click', function() {
+               botao.addEventListener('click', function() {
                 
-               if(hospitalA == 0){
-                 console.log("a")
-                hospitalA = hospitalA +1;
-                 }else{
+               if(hospitalA == hospitalB){
+               
+                      if(distanciaA > distanciaB){
+                        alert("Ambos os hospitais estao com o mesmo numero de atendimentos, Vá para o hopspital A que é masi perto. ")
+                        hospitalA = hospitalA +1;
 
-               console.log("B")
+                      }else{
+                        alert("Ambos os hospitais estao com o mesmo numero de atendimentos, Vá para o hopspital B que é masi perto. ")
+                        hospitalB = hospitalB +1;
+
+
+                      }
+   
+               
+              }else if(hospitalB < hospitalA){
+                        alert("va para o hopital B, que tem uma fila menor! ")
+                         hospitalB = hospitalB +1;
+
+                     }else{
+                        alert("Vá para o hospital A , que esta com uma fila menor")
+                        hospitalA = hospitalA +1;
                  }
 
-
                
-                })
+                
+                
+                } )
 
                 
                 
