0ecd233940b96a21dc63be308b499f7068cea481
Rodrigo Almeida <rsa.0408@hotmail.com>
2020-06-13 09:57:52 -0300

    Começando o back end

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+          
+       var nome = document.getElementById("nome");
+
+       var hospitalA;
+       var hospitalB;
+
+       hospitalA = 0;
+       hospitalB = 0;
+
+       if(hospitalA == 0){
+           console.log("Ä")
+
+       }else{
+
+            console.log("B")
+       }
+
+
+
+
+
+
+
+               var botao = document.getElementById("btn");
+
+            botao.addEventListener('click', function() {
+                
+               if(hospitalA == 0){
+                 console.log("a")
+                hospitalA = hospitalA +1;
+                 }else{
+
+               console.log("B")
+                 }
+
+
+               
+                })
+
+                
+                
+            
+            
