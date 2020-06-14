aff8c485c21c658170750409b03a9d8c94557c60
Rodrigo Almeida <rsa.0408@hotmail.com>
2020-06-14 11:57:01 -0300

    teste de algumas alteraçoes

--- dados.php
+++ dados.php
@@ -39,7 +39,7 @@ echo "<b>Nome:</b>  $nome <br/> <b>CPF:</b> $cpf";
 
             
             <div class="segunda-coluna">
-                <h2 class="tittle tittle-segundo">Seu código de atendimento é:</h2>
+                <h2 id="unidade" class="tittle tittle-segundo">Seu código de atendimento é:</h2>
                     <p>DZX56789</p>    
                      <img src="Imagens/qrcode.png" alt="">                
                     <h3 align="center"> Unidade com menor fila próximo a você:</h3> 
@@ -59,7 +59,7 @@ echo "<b>Nome:</b>  $nome <br/> <b>CPF:</b> $cpf";
 
 </div>
 </div></div>
-
+<script src="js/main.js" > </script>
 </body>
 
 </html>
