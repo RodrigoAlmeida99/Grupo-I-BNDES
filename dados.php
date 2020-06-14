60762070a1c61a0bf2645a5e540de182ec541a06
ventugus <56004576+ventugus@users.noreply.github.com>
2020-06-14 11:42:01 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Agendamento Sus Emergencia</title>
+    
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
+    <link rel="icon" href="Imagens/sus.png" type="image/png" />
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
 
